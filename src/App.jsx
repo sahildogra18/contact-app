71f9d3159bd52c5868fe3ca795c1b5fc8729759c
sahildogra18 <sahildgr18@gmail.com>
2025-06-29 03:31:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import "./App.css";
 import Read from "./components/Read";
 import Create from "./components/Create";
 import Edit from "./components/Edit";
-import Loader from "./components/Loader";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Read />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/edit" element={<Edit />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Read />} />
+        <Route path="/read" element={<Read />} /> {/* Optional */}
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
